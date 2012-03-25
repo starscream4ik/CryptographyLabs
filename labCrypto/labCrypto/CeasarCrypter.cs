3e41cb254580a340adc9b7e6567b78ebe446a7e3
Andrew Petrovich <starscream@mail.ua>
2012-03-25 15:39:09 +0300

    initial commit

---
+++ labCrypto/labCrypto/CeasarCrypter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using cryptoAlg;
+
+namespace labCrypto
+{
+    public class CeasarCrypter : Crypter
+    {
+        public CeasarCrypter(CryptoStrategy strategy)
+        {
+            setStrategy(strategy);
+        }
+    }
+}
---
+++ labCrypto/labCrypto/Crypter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using cryptoAlg;
+
+namespace labCrypto
+{
+    public abstract class Crypter
+    {
+        private CryptoStrategy cryptMethod;
+
+        public void  setStrategy(CryptoStrategy strategy)
+        {
+            this.cryptMethod = strategy;
+        }
+        
+        public String getCrypt(String message, String key)
+        {
+            return cryptMethod.getCrypt(message, key);
+        }
+
+        public String getDecrypt(String message, string key)
+        {
+            return cryptMethod.getDecrypt(message, key);
+        }
+
+    }
+}
